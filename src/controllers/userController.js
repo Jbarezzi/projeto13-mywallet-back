b4d83aed0d3afbc59901729372428183ea5e69ac
Juan Barezzi <juan.barezzi@gmail.com>
2022-07-03 17:21:24 -0300

    feat: implement user login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,34 @@
-import { db } from "./../database/mongo.js";
+import { db, objectId } from "./../database/mongo.js";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 export async function signUp(req, res) {
     const newUser = req.body;
     const isUserRegistered = await db.collection("users").findOne({ email: newUser.email });
+
     if(isUserRegistered) {
         res.status(409).send("Esse email já está cadastrado.");
         return;
     }
+
     const saltRounds = 10;
     newUser.password = bcrypt.hashSync(newUser.password, saltRounds);
     await db.collection("users").insertOne(newUser)
     res.sendStatus(201);
+}
+
+export async function login(req, res) {
+    const { email, password } = req.body;
+    const isUserValid = await db.collection("users").findOne({ email });
+    const isPasswordValid = await bcrypt.compare(password, isUserValid.password);
+    if(isUserValid && isPasswordValid) {
+        const session = {
+            token: uuid(), 
+            userId: isUserValid._id,
+        };
+        await db.collection("sessions").insertOne(session);
+        res.status(201).send({ token: session.token });
+    } else {
+        res.status(401).send("Senha ou email incorretos!");
+    }
 }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { signUp } from "./../controllers/userController.js";
+import { signUp, login } from "./../controllers/userController.js";
 import { validateSignUp } from "../middlewares/joiValidations.js";
 
 const router = Router();
 
 router.post("/register", validateSignUp, signUp);
-// router.post("/login", login);
+router.post("/login", login);
 
 export default router;
