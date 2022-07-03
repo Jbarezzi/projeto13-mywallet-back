0ba8f29df8bb1360798aca62a0c8a1f0c36f5fc4
Juan Barezzi <juan.barezzi@gmail.com>
2022-07-02 22:56:18 -0300

    feat: implement .env

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
-// import userRouter from "./routes/userRouter"
+import dotenv from "dotenv";
+import userRouter from "./routes/userRouter.js"
 // import transactionRouter from "./routes/transactionRouter"
-import { userRouter, transactionRouter } from "./routes";
+
+dotenv.config();
 const app = express();
 
 app.use(cors());
@@ -10,3 +12,6 @@ app.use(express.json());
 
 app.use(userRouter);
 app.use(transactionRouter);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
