ef4c281c6e9c1863a90f289b6fee4f6f271b662f
Juan Barezzi <juan.barezzi@gmail.com>
2022-07-03 12:28:35 -0300

    feat: implement user sign up

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,15 @@
 import { db } from "./../database/mongo";
+import bcrypt from "bcrypt";
 
-export function signUp(req, res) {
-
+export async function signUp(req, res) {
+    const newUser = req.body;
+    const isUserRegistered = await db.collection("users").findOne({ email: newUser.email });
+    if(isUserRegistered) {
+        res.status(409).send("Esse email já está cadastrado.");
+        return;
+    }
+    const saltRounds = 10;
+    newUser.password = bcrypt.hashSync(newUser.password, saltRounds);
+    await db.collection("users").insertOne(newUser)
+    res.sendStatus(201);
 }
